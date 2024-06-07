410eae80e398c2c21474b2832998d42c3730f95b
Niels-vdb <nielsvdbosch95@gmail.com>
2024-06-07 10:28:11 +0200

    FEATURE: Build an exportable logger function

---
+++ main.py
@@ -0,0 +1,24 @@
+import logging.config
+import logging.handlers
+
+from src.functions import log_handler
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    log_handler.setup_logging()
+
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("exception message")
+
+
+if __name__ == "__main__":
+    main()
